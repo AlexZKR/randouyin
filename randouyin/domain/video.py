c669c999dc8d8d36141caa1e00dd91ca530f3bec
Alex Zakrevsky <zakrevskyalex1323@gmail.com>
2025-05-13 21:49:01 +0300

    (feat) parsed video model

---
+++ randouyin/domain/video.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class ParsedVideo(BaseModel):
+    """Video, parsed from search results card
+
+    Scraper gets it first
+    """
+
+    id: int
+    image_url: str
+    duration: str
+    title: str
+    date: str
+    author: str
+    likes: int
